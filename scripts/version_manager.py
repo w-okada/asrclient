95c77d1e525fcbeab229b2329b9a8c4e5218c5c3
wok <wok@local.com>
2024-09-25 23:59:22 +0900

    add build script

---
+++ scripts/version_manager.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+import toml
+import fire
+
+from asrclient.utils.parseBoolArg import parse_bool_arg
+
+# HERE = Path(__file__).parent.absolute()
+HERE = Path("./")
+
+
+def get_version():
+    pyproject_path = "pyproject.toml"
+
+    with open(pyproject_path, "r") as file:
+        pyproject_data = toml.load(file)
+
+    return pyproject_data["tool"]["poetry"]["version"]
+
+
+def generate_version_file(alpha: bool = True):
+    alpha = parse_bool_arg(alpha)
+    version = get_version()
+    if alpha:
+        version += "-alpha"
+    with open(HERE / "asrclient" / "version.txt", "w") as f:
+        f.write(version)
+    with open(
+        HERE / "web_front" / "assets" / "gui_settings" / "version.txt",
+        "w",
+    ) as f:
+        f.write(version)
+
+
+def main():
+    fire.Fire(generate_version_file)
